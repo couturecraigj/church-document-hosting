08bbe15d41e4c7169cf4a6c101c7d901a2d5bf9e
couturecraigj <couturecraigj@gmail.com>
2018-11-15 15:43:17 -0500

    Update SW.js to actual cache requests

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,41 @@
+const CACHE_NAME = "v1";
+
+self.addEventListener("activate", function(event) {
+  var cacheKeeplist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([]);
+    })
+  );
+});
+
 self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    caches.match(event.request).then(function(resp) {
+      return (
+        resp ||
+        fetch(event.request).then(function(response) {
+          return caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        })
+      );
     })
   );
 });
