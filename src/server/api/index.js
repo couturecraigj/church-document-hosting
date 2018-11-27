ecf659d9860ad38d2faae899c0b118c07f294e59
couturecraigj <couturecraigj@gmail.com>
2018-11-27 09:31:51 -0500

    Updating Icon and logging api requests

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
+const cors = require("cors");
+import morgan from "morgan";
 import bodyParser from "body-parser";
 import signup from "./signup";
 import login from "./login";
 
+const preppedCors = cors({
+  origin: true,
+  methods: ["POST", "GET"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Credentials"
+  ],
+  preflightContinue: true,
+  credentials: true
+});
+
 const app = Router();
+app.use(morgan("combined"));
+app.options("*", preppedCors);
+app.use(preppedCors);
 app.use(bodyParser.json());
 app.use("/signup", signup);
 app.use("/login", login);
