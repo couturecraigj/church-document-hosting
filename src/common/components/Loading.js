513e12bfd49c66dbaf6616bc615cb864fd7cc16c
couturecraigj <couturecraigj@gmail.com>
2018-12-24 12:05:15 -0500

    Loading fix

--- src/common/components/Loading.js
+++ src/common/components/Loading.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 
 const LoadingContainer = styled.div``;
 
-const LoadingMessage = () => <LoadingContainer>ERROR</LoadingContainer>;
+const LoadingMessage = () => <LoadingContainer>LOADING</LoadingContainer>;
 
 export default LoadingMessage;
